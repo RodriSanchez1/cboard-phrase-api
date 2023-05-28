2e5823038d7ba7f15bea73c4790320770314d925
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-05-28 20:28:58 -0300

    Chore: App error handler

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express');
 const cors = require('cors');
 var logger = require('morgan');
 const dbConnect = require('./config/db');
+
+var createError = require('http-errors');
+
+const userRoutes = require('./api/api-services/User/routes');
 const app = express();
 
 dbConnect();
@@ -12,5 +16,22 @@ app.use(cors());
 app.use(logger('dev')); //http logger
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/user', userRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 module.exports = app;
