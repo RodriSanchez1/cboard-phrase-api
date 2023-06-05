c2bb875ef032b7b95bb71148fe9281ed8b63ab1a
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-06-04 21:08:52 -0300

    Chore: Create new communicator on sign up

--- api/api-services/User/controller.js
+++ api/api-services/User/controller.js
@@ -1,8 +1,11 @@
 const User = require('./model');
+const Communicator = require('../Communicator/model');
 
 const login = async (req, res) => {
   try {
-    const findUser = await User.findOne({ email: req.body.email }).exec();
+    const findUser = await User.findOne({ email: req.body.email })
+      .populate({ path: 'communicators categories' })
+      .exec();
     if (!findUser || findUser.password !== req.body.password)
       return res.status(404).json({ message: 'Wrong email or password' });
     return res.status(200).json(findUser);
@@ -23,9 +26,29 @@ const createUser = async (req, res) => {
 
     const user = new User(req.body);
 
+    const communicator = new Communicator({
+      name: `${req.body.name} communicator`,
+      email: req.body.email,
+      author: req.body.name,
+      rootCategory: 'c4e1f6g8h2',
+      categories: [
+        'c4e1f6g8h2',
+        'c0e4g2j6k8',
+        'r2e5s7t9a1u',
+        'd2c8f9g1h5',
+        'a0b1c2d3e4',
+        'h3e9t6k8p2',
+        's0c5i2a1l8',
+        'e9m2k6h7t3',
+      ],
+    });
+
     const newUser = await user.save();
+    const newCommunicator = await communicator.save();
+
     if (newUser) return res.status(201).json(user);
   } catch (e) {
+    console.log(e);
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
