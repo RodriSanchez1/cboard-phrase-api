5caf2602f5627921ce5f4c627f1476777da96c6f
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-06-13 22:28:38 -0300

    Chore: Add timesSpeaked function to report

--- api/api-services/Report/controller.js
+++ api/api-services/Report/controller.js
@@ -8,18 +8,17 @@ async function topUsedSentences(req, res) {
   try {
     const { userId, days, action } = req.query;
 
-    console.log(userId, days, action);
-    const fechaActual = new Date();
-    const fechaInicio = new Date(
-      fechaActual.getTime() - days * 24 * 60 * 60 * 1000
+    const currentDate = new Date();
+    const startDate = new Date(
+      currentDate.getTime() - days * 24 * 60 * 60 * 1000
     );
     const topUsedSentences = await Event.aggregate([
       {
         $match: {
           userId: userId,
           time: {
-            $gte: fechaInicio,
-            $lte: fechaActual,
+            $gte: startDate,
+            $lte: currentDate,
           },
           action: action,
         },
@@ -47,10 +46,36 @@ async function topUsedSentences(req, res) {
       },
     ]);
 
-    return res.status(200).json({ topUsedSentences });
+    const timesSpeaked = await Event.aggregate([
+      {
+        $match: {
+          userId: userId,
+          time: {
+            $gte: startDate,
+            $lte: currentDate,
+          },
+          action: action,
+        },
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: '%d/%m', date: '$time' } },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: '$_id',
+          count: 1,
+        },
+      },
+    ]);
+
+    return res.status(200).json({ topUsedSentences, timesSpeaked });
   } catch (err) {
     return res.status(409).json({
-      message: 'Error getting analytics',
+      message: 'Error getting the report',
       error: err.message,
     });
   }
